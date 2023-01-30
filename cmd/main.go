5ceaeed3fc536643096077e227139cb7f9804217
Viktor Samsonov <dezerqq26@gmail.com>
2023-01-30 15:14:39 +0300

    initialized services and repos

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	todoserver "github.com/deevins/todo-restAPI"
 	"github.com/deevins/todo-restAPI/pkg/handler"
+	"github.com/deevins/todo-restAPI/pkg/repository"
+	"github.com/deevins/todo-restAPI/pkg/service"
 	"log"
 )
 
 func main() {
-	srv := new(todoserver.Server)
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
 
+	services := service.NewService(repos)
+
+	handlers := handler.NewHandler(services)
+
+	srv := new(todoserver.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
